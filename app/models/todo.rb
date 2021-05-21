f3d8275128815d1bec9cb36b5b69200545832aef
Madhanahopal N <nmadhanagopal.2000@gmail.com>
2021-05-21 20:42:12 +0530

    Can see a single todo in /todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    "#{id}. #{display_status} #{due_date.to_s(:long)} #{todo_text} "
+  end
 end
