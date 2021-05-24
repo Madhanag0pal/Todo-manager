1bf0044403dc30730dcf2a62e72342083cb69be2
Madhanahopal N <nmadhanagopal.2000@gmail.com>
2021-05-24 23:43:21 +0530

    Add index, show, update, create methods to TodosController

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,33 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_displayable_string }.join("\n")
   end
 
   def show
     id = params[:id]
-    render plain: "The id you wanted was #{id}"
+    todo = Todo.find(id)
+    render plain: todo.to_displayable_string
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "Updated todo completed status to #{completed}"
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    newTodo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    render plain: "Hey your new todo is created with id  #{newTodo.id}"
   end
 end
