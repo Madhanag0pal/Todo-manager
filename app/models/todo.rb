a7d6fbe0e3b4d7e7a71e71318ea27236118d7cb0
Madhanahopal N <nmadhanagopal.2000@gmail.com>
2021-05-29 11:04:04 +0530

    Update todo.rb with methods from L-4 submission

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,42 @@
 class Todo < ActiveRecord::Base
+  def to_s
+    to_displayable_string
+  end
+
+  def due_today?
+    due_date == Date.today
+  end
+
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
-    "#{id}. #{display_status} #{due_date.to_s(:long)} #{todo_text} "
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.add_task(h)
+    Todo.create!(todo_text: h[:todo_text], due_date: Date.today + h[:due_in_days], completed: false)
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.completed(status = true)
+    all.where(completed: status)
+  end
+
+  def self.mark_as_complete!(id)
+    todo = find(id)
+    todo.completed = !todo.completed
+    todo.save!
+    todo
   end
 end
