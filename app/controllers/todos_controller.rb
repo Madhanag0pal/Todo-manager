93c91b6b556361cf0ab86e80608d2a3dfdac6eef
Madhanahopal N <nmadhanagopal.2000@gmail.com>
2021-05-29 20:49:40 +0530

    update todo route

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,6 +3,7 @@ class TodosController < ApplicationController
 
   def index
     # render plain: Todo.order(:due_date).map { |todo| todo.to_displayable_string }.join("\n")
+    render "index"
   end
 
   def show
