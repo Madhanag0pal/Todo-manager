c7dfabbc5124189c6f2debc2477dd9e7c8ea7d91
Madhanahopal N <nmadhanagopal.2000@gmail.com>
2021-06-07 00:23:57 +0530

    Update Migration Todo and User

--- db/migrate/20210606184017_create_todo.rb
+++ db/migrate/20210606184017_create_todo.rb
@@ -1,9 +1,10 @@
-class CreateTodos < ActiveRecord::Migration[6.1]
+class CreateTodo < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
       t.text :todo_text
       t.date :due_date
       t.boolean :completed
+      t.bigint :user_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create!(first_name: "Madhan", last_name: "N", email: "madhan@email.com", password_digest: "pass")
+u2 = User.create!(first_name: "test", email: "test@email.com", password_digest: "test")
+
+Todo.create!(todo_text: "File taxes", due_date: Date.today, completed: false, user_id: u1.id)
+Todo.create!(todo_text: "Submit assignment", due_date: Date.today + 1, completed: false, user_id: u1.id)
+
+Todo.create!(todo_text: "File taxes", due_date: Date.today, completed: false, user_id: u2.id)
+Todo.create!(todo_text: "Submit assignment", due_date: Date.today + 1, completed: false, user_id: u2.id)
