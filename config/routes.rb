3ae7d6b23b9c9227475983c050e0092a6c9f281c
Madhanagopal <nmadhanagopal.2000@gmail.com>
2021-06-18 13:31:44 +0530

    config sidekiq into rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # get "todos", to: "todos#index"
-  # post "todos/create", to: "todos#create"
-  # get "todos/:id", to: "todos#show"
+  require "sidekiq/web"
+  mount Sidekiq::Web => "/sidekiq"
 
   get "/" => "home#index"
 
